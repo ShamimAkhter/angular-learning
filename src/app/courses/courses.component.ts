5b2324bbccc5a097a77c02ac489ad9d767ef730a
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-11 01:55:48 +0530

    Template variables

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,41 +3,19 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'courses',
   template: `
-    <!-- <button (click)="onSave()">Save</button> -->
+    <!-- <input (keyup.enter)="onKeyUp($event)"> Approach 1 -->
     
-    <!-- <button (click)="onSave($event)">Save</button> -->
-    
-    <!-- <div (click)="onDiv1Clicked()">
-      <div (click)="onDiv2Clicked()">
-        <button (click)="onSave($event)">Save</button>
-      </div>
-    </div> -->
-    
-    <!-- <input (keyup)="onKeyUp($event)"> better way: applying a filter -->
-    <input (keyup.enter)="onKeyUp()"> 
+    <input #email (keyup.enter)="onKeyUp(email.value)"> <!-- Approach 2 -->
   `
 })
 export class CoursesComponent {
-  // onKeyUp(e:KeyboardEvent) {
-  //   if(e.keyCode === 13) console.log('ENTER was pressed!');
+  // Approach 1:
+  // onKeyUp(e:Event) {
+  //   console.log((e.target as HTMLInputElement).value);
   // }
 
-  onKeyUp() { // better way: applying a filter
-    console.log('ENTER was pressed!');
-  }
-
-  onDiv1Clicked() {
-    console.log('Div 1 was clicked');
-  }
-
-  onDiv2Clicked() {
-    console.log('Div 2 was clicked');
-  }
-
-  onSave($event: Event) {
-    // to prevent event bubbling
-    $event.stopPropagation();
-
-    console.log('Button was clicked!', $event);
+  // Approach 2:
+  onKeyUp(email: string) { // better way: applying a filter
+    console.log(email);
   }
 }
