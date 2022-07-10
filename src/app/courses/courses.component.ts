bb6b11a75e7f9222d570aec4187f0248d3ffb174
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-10 23:26:35 +0530

    Event binding and filtering

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,9 +3,41 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'courses',
   template: `
-    <button [style.backgroundColor]="isActive ? 'blue' : 'white'">Save</button>
+    <!-- <button (click)="onSave()">Save</button> -->
+    
+    <!-- <button (click)="onSave($event)">Save</button> -->
+    
+    <!-- <div (click)="onDiv1Clicked()">
+      <div (click)="onDiv2Clicked()">
+        <button (click)="onSave($event)">Save</button>
+      </div>
+    </div> -->
+    
+    <!-- <input (keyup)="onKeyUp($event)"> better way: applying a filter -->
+    <input (keyup.enter)="onKeyUp()"> 
   `
 })
 export class CoursesComponent {
-  isActive = true;
+  // onKeyUp(e:KeyboardEvent) {
+  //   if(e.keyCode === 13) console.log('ENTER was pressed!');
+  // }
+
+  onKeyUp() { // better way: applying a filter
+    console.log('ENTER was pressed!');
+  }
+
+  onDiv1Clicked() {
+    console.log('Div 1 was clicked');
+  }
+
+  onDiv2Clicked() {
+    console.log('Div 2 was clicked');
+  }
+
+  onSave($event: Event) {
+    // to prevent event bubbling
+    $event.stopPropagation();
+
+    console.log('Button was clicked!', $event);
+  }
 }
