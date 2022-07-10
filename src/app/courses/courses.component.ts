036200b2a107f319e998b32696cafda0e7e5a97c
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-11 02:28:22 +0530

    Pipes

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,18 +3,35 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'courses',
   template: `
-    <!-- <input [value]="email" (keyup.enter)="assign($event); onKeyUp()"> Approach 1 -->
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()">
+    {{ course.title }} <br>
+    {{ course.title | uppercase }} <br>
+    {{ course.title | uppercase | lowercase }} <br>
+    <hr>
+    {{ course.students }} <br>
+    {{ course.students | number }} <br>
+    <hr>
+    {{ course.rating | number }} <br>
+    {{ course.rating | number:'1.2-2' }} <br>
+    {{ course.rating | number:'1.2-2' }} <br>
+    {{ course.rating | number:'2.1-1' }} <br>
+    <hr>
+    {{ course.price }} <br>
+    {{ course.price | currency }} <br>
+    {{ course.price | currency:'INR' }} <br>
+    {{ course.price | currency:'INR':false }} <br>
+    {{ course.price | currency:'INR':false:'3.2-2' }} <br>
+    <hr>
+    {{ course.releaseDate }} <br>
+    {{ course.releaseDate | date }} <br>
+    {{ course.releaseDate | date:'shortDate' }} <br>
   `
 })
 export class CoursesComponent {
-  email = 'me@example.com';
-
-  assign(e: Event) {
-    this.email = (e.target as HTMLInputElement).value;
-  }
-
-  onKeyUp() {
-    console.log(this.email);
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9745,
+    students: 30123,
+    price: 190.95,
+    releaseDate: new Date(2022, 7, 11)
   }
 }
