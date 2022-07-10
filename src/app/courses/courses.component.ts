87b6ec000ffe051e4cfb30e97fa257ff908f33bb
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-10 22:03:04 +0530

    Templates

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,15 +1,14 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'courses', // with components we can extend HTML vocabulary
-  template: '<h2>Courses</h2>'
+  selector: 'courses', 
+  // template: '<h2>{{ "Title: " + title }}</h2>' // string interpolation
+  template: '<h2>{{ "Title: " + getTitle() }}</h2>'
 })
 export class CoursesComponent {
+  title = "List of courses";
   
+  getTitle() {
+    return this.title;
+  }
 }
-/*
-In CSS if we want to reference a selector, like
-element like             <courses>            : 'courses',
-element that has a class <div class="courses">: '.courses'
-element that has an id   <div id="courses">   : '#courses'
-*/
