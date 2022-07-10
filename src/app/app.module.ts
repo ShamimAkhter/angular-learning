274072f89840e2dcb465ab7141f8854a544893f4
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-10 22:24:25 +0530

    Services and DI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course/course.component';
+import { CoursesService } from './courses.service';
 import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
@@ -16,7 +17,9 @@ import { CoursesComponent } from './courses/courses.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
