ab820d829361a6954116291d93c722bee83fbe2a
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-11 17:06:56 +0530

    ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,11 @@ import { FavoriteChangeEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = 'map';
+  courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' },
+    { id: 4, name: 'course4' },
+    { id: 5, name: 'course5' },
+  ]
 }
