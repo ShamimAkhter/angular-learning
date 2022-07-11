fd8d3a9938da8b72f78a5e882aac6f7debdb7982
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-11 17:50:36 +0530

    ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,5 @@ import { FavoriteChangeEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses: { id: number; name: string }[] = [];
-
-  addCourse() {
-    this.courses = [
-      { id: 1, name: 'course1' },
-      { id: 2, name: 'course2' },
-      { id: 3, name: 'course3' },
-    ];
-  }
-
-  removeCourse() {
-    this.courses = [];
-  }
+  canSave = false;
 }
