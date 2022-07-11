cbbc2756dcffd18efbc135b33b5d4e7b80e5b5d6
Shamim Akhter <shamimakhter25@outlook.com>
2022-07-11 18:13:03 +0530

    Custom directives

---
+++ src/app/input-format.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+  // @Input('format') format='';
+  @Input('appInputFormat') format='';
+  
+  constructor(private el: ElementRef) { }
+  
+  // @HostListener('focus') onFocus() {
+  //   console.log('on Focus');
+  // }
+
+  @HostListener('blur') onBlur() {
+    let value: string = this.el.nativeElement.value;
+    // this.el.nativeElement.value = value.toLowerCase();
+    
+    if(this.format=='lowercase')
+      this.el.nativeElement.value = value.toLowerCase();
+      else
+      this.el.nativeElement.value = value.toUpperCase();
+  }
+  
+
+}
